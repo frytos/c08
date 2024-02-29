c6b02b4730a104f7c97fd5c6c67cd16548a067f0
Frytos <frytos@protonmail.com>
2024-02-29 01:11:45 +0100

    adds ex01 header ft_boolean.h

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -6,61 +6,27 @@
 /*   By: argrouss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 20:02:12 by argrouss          #+#    #+#             */
-/*   Updated: 2024/02/28 20:37:59 by argrouss         ###   ########.fr       */
+/*   Updated: 2024/02/29 01:09:26 by argrouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#IFNDEF FT_BOOLEAN_H
-# DEFINE FT_BOOLEAN_H
-
-# DEFINE TRUE 1
-# DEFINE FALSE 0
-# DEFINE ODD_MSG "I have an odd number of arguments."
-# DEFINE EVEN_MSG "I have an even number of arguments."
-# DEFINE SUCCESS "\n"
-# DEFINE EVEN(nbr) (nbr % 2 == 0)
-
-typedef int t_bool
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
 
 #include <unistd.h>
 
+//# define TRUE 1
+//# define FALSE 0
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define SUCCESS 0
+# define EVEN(nbr) (nbr % 2 == 0)
 
-#ENDIF
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#include "ft_boolean.h"
-void ft_putstr(char *str)
+typedef enum 
 {
-while (*str)
-write(1, str++, 1);
-}
-t_bool ft_is_even(int nbr)
-{
-return ((EVEN(nbr)) ? TRUE : FALSE);
-}
-int main(int argc, char **argv)
-{
-(void)argv;
-if (ft_is_even(argc - 1) == TRUE)
-ft_putstr(EVEN_MSG);
-else
-ft_putstr(ODD_MSG);
-return (SUCCESS);
-}
+	FALSE,
+	TRUE
+} t_bool;
+
+#endif
+
